789ceb4916247a102a9b638f37fd91728cdd9e3c
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-09-16 12:27:21 -0700

    removed tutorial code from QS File

--- SortingAlgorithms/quick_sort.py
+++ SortingAlgorithms/quick_sort.py
@@ -1,32 +1,3 @@
-# def partition(xs, start, end):
-#     follower = leader = start
-#     while leader < end:
-#         if xs[leader] <= xs[end]:
-#             xs[follower], xs[leader] = xs[leader], xs[follower]
-#             follower += 1
-#         leader += 1
-#     xs[follower], xs[end] = xs[end], xs[follower]
-#     return follower
-#
-#
-# def _quicksort(xs, start, end):
-#     if start >= end:
-#         return
-#     p = partition(xs, start, end)
-#     _quicksort(xs, start, p - 1)
-#     _quicksort(xs, p + 1, end)
-#
-#     return xs
-#
-#
-# def quicksort(xs):
-#     _quicksort(xs, 0, len(xs) - 1)
-#
-#     return xs
-
-
-from pdb import set_trace
-
 def quicksort(A: list) -> list:
     return _quicksort(A, 0, len(A) - 1)
 
